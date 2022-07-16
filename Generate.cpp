fc8e6d695eeeecca01e6bf1ad1c0b678477b3c25
lomnom <zhaoxiong.ang@gmail.com>
2022-07-16 23:09:59 +0800

    fix triangle being half

--- Generate.cpp
+++ Generate.cpp
@@ -8,9 +8,11 @@ using std::endl;
 using std::fill_n;
 
 int main(){
+	//define pitch to synthesize
 	Pitch pitch=Pitch(chr2midi('D','4'),440);
 	output << pitch.freq << "freq\n";
 
+	//define waveforms
 	frame_t squareLUT[RESOLUTION]={0};
 	fill_n(squareLUT, RESOLUTION, 255);
 	Waveform square=Waveform(squareLUT,true);
@@ -36,27 +38,31 @@ int main(){
 
 	frame_t triangleLUT[RESOLUTION]={0};
 	for (uint frame=0;frame<firstHalfSz;frame++){
-		triangleLUT[frame]=frameStep*(frame);
+		triangleLUT[frame]=2*frame_t(frameStep*(frame));
 	}
 	for (uint frame=0;frame<secHalfSz;frame++){
-		triangleLUT[frame+firstHalfSz]=frameStep*(secHalfSz-frame);
+		triangleLUT[frame+firstHalfSz]=2*frame_t(frameStep*(secHalfSz-frame));
 	}
 	Waveform triangle=Waveform(triangleLUT,true);
 
+	//define tone to synthesize
 	Wave tone=Wave(pitch,&triangle);
 
-	for (uint p=0; p<tone.frames; p++){
+	//display debug information on waveforms
+	for (uint p=0; p<tone.frames; p++){ //print information on wave lookup table
 		output << tone.fLUT[p] << " <(LUT) " << p << " <(index) " 
 		       << (uint)tone.form->phase[tone.fLUT[p]] << " <(value)" << endl;
 	}
 	output << "ENDLUT" << endl;
 
+	//display information on resulting waveform
 	for (uint n=0; n<50; n++)
 		output << tone.wavepos << " <(wave) " 
 		       << tone.onsecondphase << " <(sp) " << (uint)tone.getFrame() << " <(frame) " << endl;
 
 	output << "Generating 15 seconds of waveform..." << endl;
 
+	//send wave to stdout
 	for (uint n=0; n<RATE*15;n++){
 		speaker << tone.getFrame();
 	}
